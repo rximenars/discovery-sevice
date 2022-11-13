a5cfaf73d6aac6442f215ee84110de27f8d74464
rximenars <ximis.ximena@gmail,com>
2022-11-13 13:30:56 -0500

    Spring base, discovery service

---
+++ src/test/java/co/edu/unisabana/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.unisabana.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
